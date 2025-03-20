18a5e89066f2a814e4fba03ca1eac6d8d503368c
thunder-thigh <aashubaghel08@gmail.com>
2025-03-20 22:55:23 +0530

    rewriting all code

--- mkdirfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir /etc/.EyeLight
-touch /etc/.EyeLight/day.txt /etc/.EyeLight/night.txt /etc/.EyeLight/transition.txt
-echo "5000" | cat > /etc/.EyeLight/day.txt
-echo "3500" | cat > /etc/.EyeLight/transition.txt
-echo "2000" | cat > /etc/.EyeLight/night.txt
-
-
