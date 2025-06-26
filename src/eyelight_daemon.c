6cef40fad83b84eeb4009d5c7dbcc65e11945a67
Thunder <aashubaghel08@gmail.com>
2025-06-27 00:16:11 +0530

    daemonize function added

--- src/eyelight_daemon.c
+++ src/eyelight_daemon.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-#include <pwd.h>
+#include <time.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <pwd.h>
+
 
 #define STEP 50
 #define	STEP_DELAY_MS 50
@@ -17,6 +19,21 @@ typedef struct temps{
 	int night_temp;
 };
  
+void daemonize() {
+    pid_t pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS); // Parent exits
+
+    // Child continues
+    umask(0);
+    setsid();
+    chdir("/");
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
+
 int update_temp(){
 //    struct passwd *pw = getpwuid(getuid());
     if (pw == NULL){
